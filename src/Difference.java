1a4fa91513e6b17d49799413eeb53bb960b53b89
Abid Hussain <hussainabid7199@gmail.com>
2022-12-13 16:57:18 +0530

    Simple Addition Substraction and Multiplication

---
+++ src/Difference.java
@@ -0,0 +1,14 @@
+public class Difference {
+    public static void main(String args[]) {
+        int a, b, c, d, e;
+        a = 30;
+        b = 20;
+        c = a+b;
+        d = a-b;
+        e = a*b;
+
+        System.out.println("The sum of two number "+ a + " and "+ b +" =" + c);
+        System.out.println("The difference of two number "+ a + " and "+ b +" =" + d);
+        System.out.println("The product of two number "+ a + " and "+ b +" =" + e);
+    }
+}
