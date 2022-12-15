40b5e4b913f2f323fa05d1e8f435482cfd096f52
Abid Hussain <hussainabid7199@gmail.com>
2022-12-15 18:07:50 +0530

    Simple Addition of Two Number  Using Scanner Class

---
+++ src/Addition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Addition {
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+        System.out.println("Enter two number to perform addition");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+
+        System.out.println("The addition of "+ a +" and "+ b +" = "+ c);
+    }
+  }
+}
