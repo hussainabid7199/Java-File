afdea153bd83fae4e664e53240e7b8c4c2435412
Abid Hussain <hussainabid7199@gmail.com>
2022-12-15 16:45:46 +0530

    Printing Pattern Using Sysout Only

---
+++ src/Circle.java
@@ -0,0 +1,6 @@
+public class Circle {
+    public static void main(String args[]){
+        int r1, r2;
+        double ar1, ar2
+    }
+}
---
+++ src/Pattern.java
@@ -0,0 +1,20 @@
+// Write a program to print pattern using sysout (No Loops).
+// *
+// **
+// ***
+// ****
+
+
+public class Pattern {
+    public static void main(String args[]){
+       System.out.println("*");
+       System.out.println("**");
+       System.out.println("***");
+       System.out.println("****");
+
+       System.out.print("*\n");
+       System.out.print("**\n");
+       System.out.print("***\n");
+       System.out.print("****\n");
+    }
+}
