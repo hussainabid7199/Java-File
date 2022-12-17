e7af616342e8a2fb00f19a917340f97066acd240
Abid Hussain <hussainabid7199@gmail.com>
2022-12-17 19:05:09 +0530

    // Program to compare two number is = , > or < ;

---
+++ src/Comparision.java
@@ -0,0 +1,23 @@
+// Program to compare two number is = , > or < ;
+
+import java.util.*;
+public class Comparision {
+    public static void main(String args[]){
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("Enter your first number = ");
+            int first = sc.nextInt();
+            System.out.print("Enter your second number = ");
+            int second = sc.nextInt();
+
+            if(first == second){
+                System.out.println("The entered number "+ first + " is equal to " + second);
+            }else if(first > second){
+                System.out.println("Number "+ first +" is greater than "+ second);
+            }else{
+                System.out.println("Number "+ second +" is greater than "+ first);
+            }
+        }catch (Exception error){
+            System.out.println("Error Found = "+ error);
+        }
+    }
+}
