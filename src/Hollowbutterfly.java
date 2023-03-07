2432182e381da91d30e31d59f7ffdc4900e1728d
Abid Hussain <hussainabid7199@gmail.com>
2023-03-07 10:23:32 +0530

    Hollowbutterflypattern

---
+++ src/Hollowbutterfly.java
@@ -0,0 +1,50 @@
+
+public class Hollowbutterfly {
+    public static void main(String args[]) {
+        try {
+            int n = 4;
+            for(int i = 1; i<=n; i++){
+                for(int j=1; j<=i; j++){
+                    if(i==3 && j == 2 || i==4 && j == 2 || i == 4 && j == 3){
+                        System.out.print(" ");
+                    }else{
+                        System.out.print("*");
+                    }
+                }
+                for(int j=1; j<=2*(n-i); j++){
+                    System.out.print(" ");
+                }
+                for(int j=1; j<=i; j++){
+                    if(i==3 && j == 2 || i==4 && j == 2 || i == 4 && j == 3){
+                        System.out.print(" ");
+                    }else{
+                        System.out.print("*");
+                    }
+                }
+                System.out.println();
+            }
+            for(int i = n; i>=1; i--){
+                for(int j=1; j<=i; j++){
+                    if(i==3 && j == 2 || i==4 && j == 2 || i == 4 && j == 3){
+                        System.out.print(" ");
+                    }else{
+                        System.out.print("*");
+                    }
+                }
+                for(int j=1; j<=2*(n-i); j++){
+                    System.out.print(" ");
+                }
+                for(int j=1; j<=i; j++){
+                    if(i==3 && j == 2 || i==4 && j == 2 || i == 4 && j == 3){
+                        System.out.print(" ");
+                    }else{
+                        System.out.print("*");
+                    }
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
